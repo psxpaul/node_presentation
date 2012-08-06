38c1efca8c11b0589aba336d4b740460f06171ca
Paul Roberts <proberts@cj.com>
2012-08-06 16:39:37 -0700

    Initial add of the NodeJs_Presentation project

---
+++ README.md
@@ -0,0 +1,14 @@
+This is a very simple NodeJs presentation I created for a lightnight talk at work.
+
+The project uses NodeJs to save slide contents to a Mongo database, then display the slides in the browser using some pretty CSS.  You can even view the application source code by going to http://localhost:3000/app.js
+
+Requirements:
+  - NodeJs
+  - MongoDb
+
+To Run:
+  - "node app.js"
+  - go to http://localhost:3000
+  - type in some slide titles and contents
+  - click through the slides
+  - talk about interesting stuff, so people will listen
---
+++ app.js
@@ -0,0 +1,51 @@
+var express = require("express"),
+    app = express(),
+    MongoDb = require("mongodb"),
+    db = new MongoDb.Db("test", new MongoDb.Server("localhost", 27017, {auto_reconnect: true}, {})),
+    findAllSlides,
+    postSlide;
+
+app.configure(function () {
+    app.set("port", process.env.PORT || 3000);
+    app.use(express.bodyParser());
+    app.use(express["static"](__dirname));
+});
+
+db.open(function (err, db) {
+    db.collection("slides", function (error, collection) {
+        findAllSlides = function (callback) {
+            collection.find().toArray(callback);
+        };
+
+        postSlide = function (newSlide, callback) {
+            collection.save(newSlide, {safe: true}, callback);
+        };
+    });
+});
+
+// BEGIN ROUTES
+app.get("/slides", function (req, res) {
+    findAllSlides(function (error, results) {
+        if (error) {
+            res.json(error, 400);
+        } else if (!results) {
+            res.send(404);
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+app.post("/slide", function (req, res, next) {
+    postSlide(req.body, function (error, result) {
+        if (error) {
+            res.json(error, 400);
+        } else {
+            res.json(req.body, 201);
+        }
+    });
+});
+// END ROUTES
+
+app.listen(3000);
+console.log("Express server listening on port ", app.get("port"));
